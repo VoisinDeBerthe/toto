5d321dbad01351377b2d44db1b79a114472149e8
VoisinDeBerthe <150340868+VoisinDeBerthe@users.noreply.github.com>
2024-02-16 09:16:50 +0100

    Update python jour 2.py

--- python jour 2.py
+++ python jour 2.py
@@ -122,7 +122,7 @@ def ma_division(num, denom):
 
 def est_bissextile(annee):
     if annee % 4 == 0 :
-        if annee % 100 == 0
+        if annee % 100 == 0 :
             if annee // 400 == 0:
                 return True
             else:
